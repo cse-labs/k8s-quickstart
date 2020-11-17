ff4b7ff908feca30ee31c099b4eed4c1059a68cd
Bart Robertson <bartr@microsoft.com>
2020-11-17 06:45:45 +0000

    updated codespace setup

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -2,11 +2,3 @@
 
 apt-get update
 apt-get install -y --no-install-recommends git curl httpie
-
-# pull the docker images we use
-docker pull ubuntu
-docker pull golang
-docker pull mariadb
-docker pull redis
-docker pull retaildevcrew/govote
-docker pull retaildevcrew/goweb
