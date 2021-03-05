a74e637f117f94f57a693be5fb3cca6d8f159f3a
bartr <bartr@microsoft.com>
2021-03-05 02:57:42 -0600

    updated devcontainer

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -1,8 +1,43 @@
 #!/bin/sh
 
-mkdir -p ~/.ssh
-ssh-keygen -t rsa -f ~/.ssh/id_rsa
-chmod 700 ~/.ssh/id*
+# add kubectl completion
+mkdir -p ~/.local
+cp .devcontainer/kubectl_completion ~/.local/kubectl_completion
 
-sudo apt-get update
-sudo apt-get install -y --no-install-recommends git curl httpie
+# update .bashrc with helpful aliases
+pushd ~
+echo "" >> .bashrc
+echo "export PATH=$PATH:$HOME/.local/bin" >> .bashrc
+
+echo "alias k='kubectl'" >> .bashrc
+echo "alias kga='kubectl get all'" >> .bashrc
+echo "alias kgaa='kubectl get all --all-namespaces'" >> .bashrc
+echo "alias kaf='kubectl apply -f'" >> .bashrc
+echo "alias kdelf='kubectl delete -f'" >> .bashrc
+echo "alias kl='kubectl logs'" >> .bashrc
+echo "alias kccc='kubectl config current-context'" >> .bashrc
+echo "alias kcgc='kubectl config get-contexts'" >> .bashrc
+echo "alias kje='kubectl exec -it jumpbox -- '" >> .bashrc
+
+echo "export GO111MODULE=on" >> .bashrc
+echo "alias ipconfig='ip -4 a show eth0 | grep inet | sed \"s/inet//g\" | sed \"s/ //g\" | cut -d / -f 1'" >> .bashrc
+echo 'export PIP=$(ipconfig | tail -n 1)' >> .bashrc
+echo 'source $HOME/.local/kubectl_completion' >> .bashrc
+echo 'complete -F __start_kubectl k' >> .bashrc
+
+export PATH=$PATH:$HOME/.local/bin
+
+popd
+
+
+### Application specific configuration
+### Delete if reusing for other projects
+
+# create prometheus directory
+sudo mkdir -p /prometheus
+sudo chown -R 65534:65534 /prometheus
+
+# copy grafana.db to /grafana
+sudo mkdir -p /grafana
+sudo  cp deploy/grafanadata/grafana.db /grafana
+sudo  chown -R 472:472 /grafana
