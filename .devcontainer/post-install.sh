3bdfc0d62ce74857e115247c23e26889280f766a
bartr <bartr@microsoft.com>
2021-05-05 01:02:36 -0500

    use webv 2.0-beta1 to help test

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-# install WebV
+# install WebV (using the beta for testing)
 # we can't install this in the base image
-dotnet tool install -g webvalidate
+dotnet tool install -g --version 2.0.0-beta1 webvalidate
 
 # copy our grafana dashboards
 sudo cp deploy/grafana/grafana.db /grafana
