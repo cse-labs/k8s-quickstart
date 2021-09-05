46a7ef290333c3224220a900d14a23866710c59a
johee <johchung@microsoft.com>
2021-09-05 03:57:50 +0000

    Update devcontainer setup

--- .devcontainer/post-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# install WebV
-dotnet tool install -g webvalidate
-
-# copy our grafana dashboards
-sudo cp deploy/grafana/grafana.db /grafana
-sudo chown -R 472:472 /grafana
---
+++ .devcontainer/post-start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "post-start start" >> ~/status
+
+# this runs in background each time the container starts
+
+# update the base docker images
+
+echo "post-start complete" >> ~/status
